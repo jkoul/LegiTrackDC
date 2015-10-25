502be02d1b6f381fece610022756a42633b2e4bf
Michael Kalish <kalishmichael@gmail.com>
2015-10-25 14:30:05 -0400

    Adding in firebase for up/down votes and adding some view changes

--- js/services/bill.js
+++ js/services/bill.js
@@ -10,43 +10,40 @@
 
   billServices.factory('Bill', ['$resource', function($resource) {
     return $resource('http://openstates.org/api/v1/bills/?state=dc&search_window=term&fields=bill_id,title,sponsors,actions,action_dates&apikey=ef41e9a0f78e43b3b5491ff01138a442/', {});
-  }]).done(function(){
-    var firebaseUrl = "https://legitrack.firebaseio.com";
-    var connectedRef = new Firebase(firebaseUrl + "/.info/connected");
-     connectedRef.on("value", function(snapshot) {
-       if (snapshot.val() === true) {
-         console.log("Connected to firebase:", firebaseUrl);
-       } else {
-         console.log("Not connected to firebase:", firebaseUrl);
-       }
-     });
+  }]);
+  billServices.factory('Vote', ['$firebaseObject','$firebaseArray', '$routeParams', voteFirebase]);
 
-    billServices.factory('Vote', ['$firebaseObject','$firebaseArray', '$routeParams', voteFirebase]);
-    function voteFirebase($firebaseObject, $firebaseArray, $routeParams) {
+  function voteFirebase($firebaseObject, $firebaseArray, $routeParams) {
+      var firebaseUrl = "https://legitrack.firebaseio.com";
+      var connectedRef = new Firebase(firebaseUrl + "/.info/connected");
       var votesRef = new Firebase(firebaseUrl + "/votes");
       var billVotes = $firebaseArray(votesRef);
       var billVote = {
         query: function(){return billVotes;},
-        get: function(score, callback) {
-          var found = $firebaseObject(votesRef.child(score.$bill_id));
-          if(typeof callback == "function") {
-            callback(found);
-            return found;
-          } else {
-            score.save();
+        get: function(bill) {
+          var found = billVotes.find(function(vote){
+            return vote.$id === bill.bill_id;
+          });
+          if(!found) {
+            found = {
+              supporting: 0,
+              opposing: 0
+            };
+            votesRef.child(bill.bill_id).set(found);
           }
+          votesRef.child(bill.bill_id).on('value', function(snapshot){
+            bill.score = snapshot.val();
+          });
+          bill.score = found;
         },
-        save: function(score, callback){
+        save: function(bill){
           var score = {
-            supporting: 0,
-            opposing: 0
+            opposing: bill.score.opposing,
+            supporting: bill.score.supporting
           };
-          billVotes.$add(score).then(function(ref) {
-            score.$bill_id = $routeParams.bill_id;
-            callback(score);
-          })
+          votesRef.child(bill.bill_id).set(score)
         }
       }
+        return billVote;
     }
-  })
 })()
